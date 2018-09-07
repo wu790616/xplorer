8b62ee977ce6f0206c8acdf64d61e64eecf9505d
Yedda <YeddaChuang@gmail.com>
2018-09-08 00:59:03 +0800

    add bookmarks resources for create and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   resources :issues, only: [:show, :create, :edit, :update, :destroy]
 
   resources :topicfollowships, only: [:create, :destroy]
+  resources :bookmarks, only: [:create, :destroy]
 end
