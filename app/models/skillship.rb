8f666d56c7636b7bbe3472604f1eb31f925c5877
Wendy <wendy.hj.wu@gmail.com>
2018-09-07 21:27:49 +0800

    create skillship model

---
+++ app/models/skillship.rb
@@ -0,0 +1,2 @@
+class Skillship < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_07_102436) do
+ActiveRecord::Schema.define(version: 2018_09_07_132603) do
 
   create_table "notifications", force: :cascade do |t|
     t.integer "user_id"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2018_09_07_102436) do
 
   create_table "skills", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skillships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "skill_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
