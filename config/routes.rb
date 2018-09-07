e9488b93ba2cd570779c9fbc2c431c5813f5805d
Yedda <YeddaChuang@gmail.com>
2018-09-08 01:18:51 +0800

    add users resources for show and edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :users, only: [:show, :edit]
+  
   resources :topics, only: [:index, :show]
 
   resources :issues, only: [:show, :create, :edit, :update, :destroy] do
