51cc93a38b55b5e425c0d2891d81b6468bc4c122
Yedda <YeddaChuang@gmail.com>
2018-09-07 23:32:33 +0800

    add topicfollowships resource for create and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :topics, only: [:index, :show]
+
+  resources :topicfollowships, only: [:create, :destroy]
 end
