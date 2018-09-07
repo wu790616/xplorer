79f38c908232e8dcbedf7d9cd514079fec9536e1
Wendy <wendy.hj.wu@gmail.com>
2018-09-07 18:10:05 +0800

    create skill model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_07_090114) do
+ActiveRecord::Schema.define(version: 2018_09_07_100735) do
+
+  create_table "skills", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
