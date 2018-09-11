407a9f1ba7170499060c4a473912b26d900394bf
Wendy <wendy.hj.wu@gmail.com>
2018-09-11 13:53:51 +0800

    Create user edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(@user)
+    end
+  end
 end
