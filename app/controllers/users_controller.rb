91fab8428691b8d0cbac48f50a78d81d454fc6dc
Wendy <wendy.hj.wu@gmail.com>
2018-09-13 00:06:59 +0800

    remove repeat code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,25 @@
 class UsersController < ApplicationController
+   before_action :set_user, only: [:show, :edit, :update]
+
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
     if @user != current_user
       redirect_to user_path(@user)
     end
   end
 
   def update
-    @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user)
   end
   
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
   
   def user_params
     params.require(:user).permit(:name, :introduction, :avatar, :contact_privacy, :topic_follow_privacy, :issue_book_privacy, :skill_list)
