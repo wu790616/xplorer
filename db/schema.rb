e520f0234cc6c676e998b0e3f9aaa2fada1b95e9
Wendy <wendy.hj.wu@gmail.com>
2018-09-18 01:27:04 +0800

    add edit_time column to issue to record last edit time

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_17_053323) do
+ActiveRecord::Schema.define(version: 2018_09_17_171544) do
 
   create_table "ahoy_events", force: :cascade do |t|
     t.integer "visit_id"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 2018_09_17_053323) do
     t.boolean "draft", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "edit_time"
   end
 
   create_table "likes", force: :cascade do |t|
