dee90ace350f5c8d1a38de01d9df97f4b3078c65
Yedda <YeddaChuang@gmail.com>
2018-09-18 02:50:37 +0800

    draw systen xmap by topics and links in JSON format

--- app/assets/javascripts/xmap.js
+++ app/assets/javascripts/xmap.js
@@ -1,90 +1,89 @@
 function xmap(svg, topics, links, width, height) {
-	var color = d3.scaleOrdinal()
-	 							.range(d3.schemeCategory20);
-	var simulation = d3.forceSimulation()
-										 .force("link", d3.forceLink())
-										 .force("charge", d3.forceManyBody())
-										 .force("center", d3.forceCenter(width / 2, height / 2));
-	var link = svg.append("g")
-								.attr("class", "links")
-								.selectAll("line")
-								.data(links)
-								.enter().append("line")
-								.attr("stroke-width", 2)
-								.attr("stroke","black");
-	var node = svg.append("g")
-								.attr("class", "nodes")
-								.selectAll("circle")
-								.data(topics)
-								.enter().append("circle")
-								.attr("r", function(d){return d.strength;})
-								.attr("fill", function(d,i) { return color(i); })
-								.attr('stroke','white')
-								.attr('stroke-width',2)
-								.call(d3.drag()
-								.on("start", dragstarted)
-								.on("drag", dragged)
-								.on("end", dragended));
-	var text = svg.selectAll("text")
-								.data(topics)
-								.enter()
-								.append("text")
-								.style("fill", "black")
-								.attr("dx", 12)
-								.attr("dy", 5)
-								.text(function(d){return d.name;});
-	
-	simulation
-		.nodes(topics)
-		.on("tick", ticked);
-
-	simulation.force("link")
-		.links(links)
-		.distance(link_distance);
-
-	simulation.force("charge").strength(-200)
-
-	function ticked() {
-		link
-			.attr("x1", function(d) { return d.source.x; })
-			.attr("y1", function(d) { return d.source.y; })
-			.attr("x2", function(d) { return d.target.x; })
-			.attr("y2", function(d) { return d.target.y; });
-		node
-			.attr("cx", function(d) { return d.x; })
-			.attr("cy", function(d) { return d.y; });
-		text
-			.attr("x", function(d) { return d.x;})
-			.attr("y", function(d) { return d.y;});
-	};
-	
-	function dragstarted(d) {
-		if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-			d.fx = d.x;
-			d.fy = d.y;
-	}
+		var color = d3.scaleOrdinal()
+		 							.range(d3.schemeCategory20);
+		var simulation = d3.forceSimulation()
+											 .force("link", d3.forceLink())
+											 .force("charge", d3.forceManyBody())
+											 .force("center", d3.forceCenter(width / 2, height / 2));
+		var link = svg.append("g")
+									.attr("class", "links")
+									.selectAll("line")
+									.data(links)
+									.enter().append("line")
+									.attr("stroke-width", 2)
+									.attr("stroke","black");
+		var node = svg.append("g")
+									.attr("class", "nodes")
+									.selectAll("circle")
+									.data(topics)
+									.enter().append("circle")
+									.attr("r", function(d){return d.strength;})
+									.attr("fill", function(d,i) { return color(i); })
+									.attr('stroke','white')
+									.attr('stroke-width',2)
+									.call(d3.drag()
+									.on("start", dragstarted)
+									.on("drag", dragged)
+									.on("end", dragended));
+		var text = svg.selectAll("text")
+									.data(topics)
+									.enter()
+									.append("text")
+									.style("fill", "black")
+									.attr("dx", 12)
+									.attr("dy", 5)
+									.text(function(d){return d.name;});
 		
-	function dragged(d) {
-		d.fx = d3.event.x;
-		d.fy = d3.event.y;
-	}
-		
-	function dragended(d) {
-		if (!d3.event.active) simulation.alphaTarget(0);
-		d.fx = null;
-		d.fy = null;
-	}
-		
-	function link_distance(d) {
-		if(d.strength > 2048) return  30*3;
-		if(d.strength > 1024) return  40*3;
-		if(d.strength >  512) return  50*3;
-		if(d.strength >  256) return  60*3;
-		if(d.strength >  128) return  70*3;
-		if(d.strength >   64) return  80*3;
-		if(d.strength >   32) return  90*3;
-		if(d.strength >   16) return 100*3;
-		if(d.strength >    8) return 110*3;
-	};
-			
+		simulation
+			.nodes(topics)
+			.on("tick", ticked);
+
+		simulation.force("link")
+		  .links(links)
+			.distance(link_distance);
+
+		simulation.force("charge").strength(-200);
+
+		function ticked() {
+			link
+				.attr("x1", function(d) { return d.source.x; })
+				.attr("y1", function(d) { return d.source.y; })
+				.attr("x2", function(d) { return d.target.x; })
+				.attr("y2", function(d) { return d.target.y; });
+			node
+				.attr("cx", function(d) { return d.x; })
+				.attr("cy", function(d) { return d.y; });
+			text
+				.attr("x", function(d) { return d.x;})
+				.attr("y", function(d) { return d.y;});
+		};
+
+		function dragstarted(d) {
+			if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+				d.fx = d.x;
+				d.fy = d.y;
+		};
+
+		function dragged(d) {
+			d.fx = d3.event.x;
+			d.fy = d3.event.y;
+		};
+
+		function dragended(d) {
+			if (!d3.event.active) simulation.alphaTarget(0);
+			d.fx = null;
+			d.fy = null;
+		};
+
+		function link_distance(d) {
+			if(d.strength > 2048) return  30*3;
+			if(d.strength > 1024) return  40*3;
+			if(d.strength >  512) return  50*3;
+			if(d.strength >  256) return  60*3;
+			if(d.strength >  128) return  70*3;
+			if(d.strength >   64) return  80*3;
+			if(d.strength >   32) return  90*3;
+			if(d.strength >   16) return 100*3;
+			if(d.strength >    8) return 110*3;
+		};
 }
