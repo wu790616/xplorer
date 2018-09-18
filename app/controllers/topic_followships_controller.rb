c59e52eb86ebc6deb16d8efaa8f45a5dc7ab71af
Yedda <YeddaChuang@gmail.com>
2018-09-18 20:44:39 +0800

    add create and destroy in TopicFollowshipXontroller

--- app/controllers/topic_followships_controller.rb
+++ app/controllers/topic_followships_controller.rb
@@ -1,2 +1,17 @@
 class TopicFollowshipsController < ApplicationController
+  def create
+    @followship = current_user.topic_followships.create(topic_id: params[:topic_id])
+
+    if @followship.save
+      redirect_back(fallback_location: root_path)
+    else
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def destroy
+    @followship = current_user.topic_followships.where(topic_id: params[:topic_id]).first
+    @followship.destroy
+    redirect_back(fallback_location: root_path)
+  end
 end
