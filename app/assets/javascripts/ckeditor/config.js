1fa478f2642f426f97c59c3a579888202843925a
Wendy <wendy.hj.wu@gmail.com>
2018-09-23 15:49:36 +0800

    add divarea plugin for ckeditor to resolve Error code: editor-destroy-iframe

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -33,11 +33,11 @@ CKEDITOR.editorConfig = function( config )
 
   config.allowedContent = true;
 
-  config.format_tags = 'p;h1;h2;h3'
+  config.format_tags = 'p;h1;h2;h3';
 
-  config.extraPlugins = 'embed', 'embedbase', 'widget', 'notificationaggregator', 'widgetselection', 'lineutils';
+  config.extraPlugins = 'embed,embedbase,widget,lineutils,notificationaggregator,widgetselection,divarea';
   
-  config.embed_provider = '//ckeditor.iframe.ly/api/oembed?url={url}&callback={callback}'
+  config.embed_provider = '//ckeditor.iframe.ly/api/oembed?url={url}&callback={callback}';
 
   // Toolbar groups configuration.
   config.toolbar = [
