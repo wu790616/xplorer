52ab97edc79ad0a9d8a84d8fd593950bc6a261d9
Yedda <YeddaChuang@gmail.com>
2018-09-28 01:44:12 +0800

    layer 1 system xmap

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -12,7 +12,7 @@ class Topic < ApplicationRecord
     self.taged_issues.include?(issue)
   end
 
-  def system_map(base, scale, page)
+  def system_map(base, scale, page, current_user)
     topics = []
     links = []
     xmap = []
@@ -31,20 +31,37 @@ class Topic < ApplicationRecord
       end
 
       next_page = (page == 0) ? 1 : 0
-      topics.push({name: "#{self.name}", base: "#{self.id}", center: "#{self.id}", from: self.id, page: page      ,strength: 2000})
-      topics.push({name: "#{link1.name }", base: "#{base.id}"  , center: "#{link1.id }", from: self.id, page: 0         ,strength: 1000}) unless (link1 == nil)
-      topics.push({name: "#{link2.name }", base: "#{base.id}"  , center: "#{link2.id }", from: self.id, page: 0         ,strength: 1000}) unless (link2 == nil)
-      topics.push({name: "#{link3.name }", base: "#{base.id}"  , center: "#{link3.id }", from: self.id, page: 0         ,strength: 1000}) unless (link3 == nil)
-      topics.push({name: "#{link4.name }", base: "#{base.id}"  , center: "#{link4.id }", from: self.id, page: 0         ,strength: 1000}) unless (link4 == nil)
-      topics.push({name: "Re-generate"   , base: "#{base.id}"  , center: "#{self.id}", from: self.id, page: next_page ,strength: 2000}) unless (self.links_count <= 4)
+      topics.push({name: "#{ self.name}", base: "#{self.id}", center: "#{ self.id}", from: self.id, page: page      ,strength: 2000})
+      topics.push({name: "#{link1.name}", base: "#{base.id}", center: "#{link1.id}", from: self.id, page: 0         ,strength: 1000}) unless (link1 == nil)
+      topics.push({name: "#{link2.name}", base: "#{base.id}", center: "#{link2.id}", from: self.id, page: 0         ,strength: 1000}) unless (link2 == nil)
+      topics.push({name: "#{link3.name}", base: "#{base.id}", center: "#{link3.id}", from: self.id, page: 0         ,strength: 1000}) unless (link3 == nil)
+      topics.push({name: "#{link4.name}", base: "#{base.id}", center: "#{link4.id}", from: self.id, page: 0         ,strength: 1000}) unless (link4 == nil)
+      topics.push({name: "Re-generate"  , base: "#{base.id}", center: "#{ self.id}", from: self.id, page: next_page ,strength: 2000}) unless (self.links_count <= 4)
 
       links.push({source: 0, target:1}) unless (link1 == nil)
       links.push({source: 0, target:2}) unless (link2 == nil)
       links.push({source: 0, target:3}) unless (link3 == nil)
       links.push({source: 0, target:4}) unless (link4 == nil)
+   #end
+    elsif (scale == 1) # one layer
+      layer1 = XplorerMap.where(from_id: self.id)
 
-      xmap.push({topics: topics})
-      xmap.push({links: links})
+      topics.push({name: self.name, base: self.id, center: self.id, from: self.id, page: 0, strength: 50})
+
+      layer1.count.times do |i|
+        if current_user
+          if current_user.followingtopic?(layer1[i])
+            topics.push({name: Topic.find(layer1[i].to_id).name, base: layer1[i].to_id, center: layer1[i].to_id, from: layer1[i].to_id, page: 0, strength: 200})
+          else
+            topics.push({name: Topic.find(layer1[i].to_id).name, base: layer1[i].to_id, center: layer1[i].to_id, from: layer1[i].to_id, page: 0, strength: 50})
+          end
+        else
+          topics.push({name: Topic.find(layer1[i].to_id).name, base: layer1[i].to_id, center: layer1[i].to_id, from: layer1[i].to_id, page: 0, strength: 50})
+        end
+        links.push({source: 0, target:i+1})
+      end
     end
+    xmap.push({topics: topics})
+    xmap.push({links: links})
   end
 end
