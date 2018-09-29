0836a8e7e9d8ec540111a068902800e4ce7b569c
Wendy <wendy.hj.wu@gmail.com>
2018-09-29 16:28:46 +0800

    setup default notifications route and page, setup notification default flow

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,5 @@
 class Notification < ApplicationRecord
+  after_commit -> { NotificationRelayJob.perform_later(self) }
   belongs_to :user
   belongs_to :recipient, class_name: "User"
   belongs_to :notifiable, polymorphic: true
