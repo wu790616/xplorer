21e796bba10d498c9d06043324c7d66ab483d1e7
Wendy <wendy.hj.wu@gmail.com>
2018-09-30 11:36:56 +0800

    User like notification

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,12 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :issue, :counter_cache => true
+
+  after_create_commit { notify }
+
+  private
+
+  def notify
+    Notification.create( recipient: self.issue.user, user: self.user,  action: "liked", notifiable: self.issue, content: self.issue.title )
+  end
 end
