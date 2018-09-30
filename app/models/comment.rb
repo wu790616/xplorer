6b43da7c0019d2bb1b40e2613fb51d941d30f40a
Wendy <wendy.hj.wu@gmail.com>
2018-09-30 12:33:54 +0800

    User comment notification

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,12 @@ class Comment < ApplicationRecord
 
   # 一個Comment有多個Reply
   has_many :replies, dependent: :destroy
+
+  after_create_commit { notify }
+
+  private
+
+  def notify
+    Notification.create( recipient: self.issue.user, user: self.user,  action: "commented", notifiable: self.issue, content: self.issue.title )
+  end
 end
