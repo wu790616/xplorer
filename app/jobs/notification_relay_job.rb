4705c10b179011a618c8813a2c29c44fcf095347
Wendy <wendy.hj.wu@gmail.com>
2018-09-30 15:35:56 +0800

    Notification will display in notifications page real time

--- app/jobs/notification_relay_job.rb
+++ app/jobs/notification_relay_job.rb
@@ -3,8 +3,7 @@ class NotificationRelayJob < ApplicationJob
 
   def perform(notification)
     # Do something later
-    # html = ApplicationController.render partial: "notifications/#{notification.notifiable_type.underscore.pluralize}/#{notification.action}", locals: {notification: notification}, formats: [:html]
-    html = "successed"
+    html = ApplicationController.render partial: "notifications/#{notification.notifiable_type.underscore.pluralize}/#{notification.action}", locals: {notification: notification}, formats: [:html]
     ActionCable.server.broadcast "notifications:#{notification.recipient_id}", html: html
   end
 end
