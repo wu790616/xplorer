8973d3ea2a24295782048469b300c8405c898078
Wendy <wendy.hj.wu@gmail.com>
2018-09-30 12:47:31 +0800

    User reply notification

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,4 +1,12 @@
 class Reply < ApplicationRecord
   belongs_to :user
   belongs_to :comment
+
+  after_create_commit { notify }
+
+  private
+
+  def notify
+    Notification.create( recipient: self.comment.user, user: self.user,  action: "replied", notifiable: self.comment, content: self.comment.issue.title )
+  end
 end
