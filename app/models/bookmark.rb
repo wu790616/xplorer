77e40ddc6ebedfd577f862e58c5fff706bfc945a
Wendy <wendy.hj.wu@gmail.com>
2018-09-30 12:24:14 +0800

    User bookmark notification, modify notification language to chinese

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,12 @@
 class Bookmark < ApplicationRecord
   belongs_to :user
   belongs_to :issue, :counter_cache => true
+
+  after_create_commit { notify }
+
+  private
+
+  def notify
+    Notification.create( recipient: self.issue.user, user: self.user,  action: "bookmarked", notifiable: self.issue, content: self.issue.title )
+  end
 end
