422e74002e6e726504ca52f80323d21794278df8
Wendy <wendy.hj.wu@gmail.com>
2018-10-01 16:43:26 +0800

    add new label for unread notification

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,8 @@
 class Notification < ApplicationRecord
   after_commit -> { NotificationRelayJob.perform_later(self, Notification.count) }
+  scope :unread, -> { where( :read => false ) }
+  scope :read, -> { where( :read => true ) }
+
   belongs_to :user
   belongs_to :recipient, class_name: "User"
   belongs_to :notifiable, polymorphic: true
