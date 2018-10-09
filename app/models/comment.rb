cfa9bdbfd02e9847a8524c353a7d87f4e3dcb529
Wendy <wendy.hj.wu@gmail.com>
2018-10-09 16:17:55 +0800

    remove UrlHelper in comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,7 +10,6 @@ class Comment < ApplicationRecord
   private
 
   def notify
-    Notification.create( recipient: self.issue.user, user: self.user,  action: "commented", notifiable: self.issue, content: self.issue.title, 
-    link: Rails.application.routes.url_helpers.issue_path(self.issue) )
+    Notification.create( recipient: self.issue.user, user: self.user,  action: "commented", notifiable: self.issue, content: self.issue.title )
   end
 end
